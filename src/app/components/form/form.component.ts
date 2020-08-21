297fc84c97545e0e055b6a0ac302b9fccf095ebe
kmolik <48009091+kmolik@users.noreply.github.com>
2020-08-21 20:01:28 +0200

    reactive form working submit

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -17,12 +17,12 @@ export class FormComponent implements OnInit {
   registerForm: FormGroup;
 
   trigger: Trigger[] = [
-    {value: 'trigger-0', viewValue: 'Planned Personal Data transfer to other processing entity'},
-    {value: 'trigger-1', viewValue: 'Planned Personal Data transfer processing entity'},
-    {value: 'trigger-2', viewValue: 'Data Collection/Aquisition Instatnt'},
-    {value: 'trigger-3', viewValue: 'Planned Change or new personal data processing purpose'},
-    {value: 'trigger-4', viewValue: 'Change(d) or new personal data processing purpose'},
-    {value: 'trigger-5', viewValue: 'Purpose/Service start/commencement date -legal/contract start'}
+    {value: 'Planned Personal Data transfer to other processing entity', viewValue: 'Planned Personal Data transfer to other processing entity'},
+    {value: 'Planned Personal Data transfer processing entity', viewValue: 'Planned Personal Data transfer processing entity'},
+    {value: 'Data Collection/Aquisition Instatnt', viewValue: 'Data Collection/Aquisition Instatnt'},
+    {value: 'Planned Change or new personal data processing purpose', viewValue: 'Planned Change or new personal data processing purpose'},
+    {value: 'Change(d) or new personal data processing purpose', viewValue: 'Change(d) or new personal data processing purpose'},
+    {value: 'Purpose/Service start/commencement date -legal/contract start', viewValue: 'Purpose/Service start/commencement date -legal/contract start'}
   ];
 
   constructor(
@@ -50,6 +50,7 @@ export class FormComponent implements OnInit {
   }
 
   onSubmit() {
+    console.log(this.registerForm.value);
   }
 
   backToList() {
